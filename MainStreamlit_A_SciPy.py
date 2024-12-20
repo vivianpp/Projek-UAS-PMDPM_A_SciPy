a6200631a56bc689109d4ab5469c66865f2d25ee
vivianpp <220711795@students.uajy.ac.id>
2024-12-20 23:17:02 +0700

    Update MainStreamlit_A_SciPy.py

--- MainStreamlit_A_SciPy.py
+++ MainStreamlit_A_SciPy.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r'C:\Users\ASUS\Documents\PMDPM\Projek UAS PMDPM_A_SciPy\BestModel_MobileNet_SciPy.h5')
+model = load_model(r'BestModel_MobileNet_SciPy.h5')
 class_names = ['Merah', 'Kuning', 'Hijau']
 
 def classify_image(image_path):
